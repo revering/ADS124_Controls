a934d11c33e56ca3bb3407af3c7b8db6de296de1
Michael Revering <rever025@umn.edu>
2017-10-09 14:09:59 -0500

    Started creating EEProm

--- EEPROM.py
+++ EEPROM.py
@@ -1,7 +1,7 @@
 import core 
 import spidev
 
-class ADS124_RPiBasic(core.ADS124_connection):
+class EEPROM_RPiBasic(core.EEPROM_connection):
    """
    Implementation of ADS124_connection for a basic raspberry pi setup.
 
@@ -10,28 +10,28 @@ class ADS124_RPiBasic(core.ADS124_connection):
    """
 
    def __init__(self):
-      self.ADS124_connect()
+      self.EEPROM_connect()
 
-   def ADS124_transfer(self,to_send):
+   def EEPROM_transfer(self,to_send):
       """
       Transfer data over spi connection.
 
-      Sends a list of bytes to the ADS124_S08 over spi, and returns the chips 
+      Sends a list of bytes to the EEPROM over spi, and returns the chips 
       response during the message.
       """
       data = self.spi.xfer2(to_send)
       return data
 
-   def ADS124_connect(self) :
+   def EEPROM_connect(self) :
       """
-      Open an spi connection to the ADS124_S08. 
+      Open an spi connection to the EEPROM. 
    
       Settings should be customized to desired use.
       """
       self.spi = spidev.SpiDev()
-      self.spi.open(0,0)
+      self.spi.open(0,1)
       self.spi.cshigh = False
-      self.spi.mode = 0b01
+      self.spi.mode = 0b00
       self.spi.max_speed_hz = 3814
       return
 
